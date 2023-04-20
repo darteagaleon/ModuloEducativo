2b5689181967f4c6dc72e79e610a4e788d54547d
Yuliana Carretero <ycarretero8@gmail.com>
2023-04-20 12:08:57 -0500

    creacioncurso1

--- AppCursos/models.py
+++ AppCursos/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+#Se crea el modelo de la tabla intermedia Curso-Usuario de la empresa
+# class Curso_Usuario(models.Model):
+#      nombre_usuario=models.CharField(max_length=100)
+#      apellido_usuario=models.CharField(max_length=100)
+
+#      def __str__(self):
+#           return self.nombre_usuario
+
+
+# Se crea el modelo de la tabla Cursos de la empresa
+class Cursos(models.Model):
+     nombre_curso=models.CharField(max_length=100)
+     descripcion = models.CharField(max_length=500)
+     estado_curso= models.BooleanField(default=False)
+     duracion_curso = models.IntegerField( default=0)
+     #Se crea la relacion
+     # nombre_usuario= models.ForeignKey(Curso_Usuario, on_delete=models.CASCADE,null=False)
+     def __str__(self):
+          return self.nombre_curso
+     #Sirve para mostrar el nombre de la tabla en el admin sin la palabra "s"
+     class Meta:
+          verbose_name_plural = 'Cursos'
+
+
+          
