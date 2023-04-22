c71bf945a461a319d444c8f24f1161a2c11b02b2
Yuliana Carretero <ycarretero8@gmail.com>
2023-04-21 21:14:18 -0500

    creacioncurso3.1

--- AppCursos/models.py
+++ AppCursos/models.py
@@ -13,9 +13,10 @@ from django.db import models
 # Se crea el modelo de la tabla Cursos de la empresa
 class Cursos(models.Model):
      nombre_curso=models.CharField(max_length=100)
-     descripcion = models.CharField(max_length=500)
+     descripcion_curso = models.CharField(max_length=500)
      estado_curso= models.BooleanField(default=False)
-     duracion_curso = models.IntegerField( default=0)
+     duracion_curso = models.IntegerField(blank=True, null=True)
+     iconoCurso = models.ImageField(upload_to='IconosCurso', null=True) 
      #Se crea la relacion
      # nombre_usuario= models.ForeignKey(Curso_Usuario, on_delete=models.CASCADE,null=False)
      def __str__(self):
@@ -25,4 +26,29 @@ class Cursos(models.Model):
           verbose_name_plural = 'Cursos'
 
 
-          
+# Se crea el modelo de la tabla Modulos de la empresa
+class Modulos(models.Model):
+     nombre_modulo=models.CharField(max_length=100)
+     estado_modulo= models.BooleanField(default=False)
+     #Se crea la relacion a cursos
+     nombre_curso= models.ForeignKey(Cursos, on_delete=models.CASCADE,null=False)
+     def __str__(self):
+          return self.nombre_modulo
+     #Sirve para mostrar el nombre de la tabla en el admin sin la palabra "s"
+     class Meta:
+          verbose_name_plural = 'Modulos'
+
+# Se crea el modelo de la tabla Clases de la empresa
+class Clases(models.Model):
+     nombre_clase=models.CharField(max_length=100)
+     duracion_clase = models.IntegerField(blank=True, null=True)
+     contenido_clase = models.URLField(max_length=200)
+     descripcion_clase = models.CharField(max_length=500)
+     estado_clase= models.BooleanField(default=False)
+     #Se crea la relacion a modulos
+     nombre_modulo= models.ForeignKey(Modulos, on_delete=models.CASCADE,null=False)
+     def __str__(self):
+          return self.nombre_clase
+     #Sirve para mostrar el nombre de la tabla en el admin sin la palabra "s"
+     class Meta:
+          verbose_name_plural = 'Clases'
--- AppCursos/views.py
+++ AppCursos/views.py
@@ -6,8 +6,24 @@ from .models import *
 #vista para crear los cursos
 class Crear_cursos (CreateView):
     model = Cursos
-    fields = ['id', 'nombre_curso', 'descripcion', 'estado_curso', 'duracion_curso', 'Curso_Usuario']
+    fields = ['nombre_curso', 'descripcion', 'estado_curso', 'duracion_curso' ]
     template_name = 'crear_cursos.html'
     success_url = '/cursos'
 
+class Crear_modulos (CreateView):
+    model = Modulos
+    fields = ['nombre_curso','nombre_modulo', 'estado_modulo']
+    template_name = 'crear_modulos.html'
+    success_url = '/modulos'
 
+class Crear_clases (CreateView):
+    model = Clases
+    fields = ['nombre_modulo','nombre_clase', 'duracion_clase','contenido_clase','descripcion_clase','estado_clase']
+    template_name = 'crear_clases.html'
+    success_url = '/clases'
+
+#vista para listar los cursos
+class Listar_cursos (ListView):
+    model = Cursos
+    template_name = 'listar_cursos.html'
+    
