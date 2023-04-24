28192cdb99ebf07304d1d9984d92c73ad5538415
Yuliana Carretero <ycarretero8@gmail.com>
2023-04-24 16:40:43 -0500

    creacioncurso3.2

---
+++ AppCursos/forms.py
@@ -0,0 +1,20 @@
+from django import forms 
+from .models import Cursos, Modulos, Clases
+
+class Crear_cursos(forms.ModelForm):
+    class Meta:
+        model = Cursos
+        fields = ['nombre_curso', 'descripcion_curso', 'estado_curso', 'duracion_curso','iconoCurso' ]
+        labels = {
+            'nombre_curso': 'Nombre del curso',
+            'descripcion_curso': 'Descripcion del curso',
+            'estado_curso': 'Estado del curso',
+            'duracion_curso': 'Duracion del curso',
+            'iconoCurso': 'Icono del curso',
+        }
+        widgets = {
+            'nombre_curso': forms.TextInput(attrs={'class':'form-control'}),
+            'descripcion_curso': forms.TextInput(attrs={'class':'form-control'}),
+            'estado_curso': forms.CheckboxInput(attrs={'class':'form-check-input'}),
+            'duracion_curso': forms.NumberInput(attrs={'class':'form-control'}),
+        }
--- ModuloFormativo/settings.py
+++ ModuloFormativo/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "AppCursos",
     "AppUsuarios",
+    'widget_tweaks',    
 ]
 
 MIDDLEWARE = [
