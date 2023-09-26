b892c34efac92c09e98db180b61b8cfca62ad644
Yuliana Carretero <ycarretero8@gmail.com>
2023-09-25 21:17:40 -0500

    Se creo tabla de Cargos y tabla de Usuario_Cargos.

--- AppUsuarios/models.py
+++ AppUsuarios/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Cargo(models.Model):
+     nombre_cargo=models.CharField(max_length=100)
+     estado_cargo= models.BooleanField(default=False)
+     def __str__(self):
+          return self.nombre_cargo
+     #Sirve para mostrar el nombre de la tabla en el admin sin la palabra "s"
+     class Meta:
+          verbose_name_plural = 'Cargos'
+
+class Usuario_Cargo(models.Model):
+     id_usuario=models.ForeignKey(User, on_delete=models.CASCADE)
+     id_cargo=models.ForeignKey(Cargo, on_delete=models.CASCADE)
+     def __str__(self):
+          return self.id_usuario.username+" - "+self.id_cargo.nombre_cargo #Para mostrar el nombre del usuario y el cargo
+     #Sirve para mostrar el nombre de la tabla en el admin sin la palabra "s"
+     class Meta:
+          verbose_name_plural = 'Usuario_Cargo'
