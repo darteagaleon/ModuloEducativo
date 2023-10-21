dc6eba8678279167a87b5df84b06278922d09791
alejo355 <117774931+alejo355@users.noreply.github.com>
2023-10-21 09:19:13 -0500

    se integro ver el material de apoyo en el nav de usuarios

--- AppUsuarios/views.py
+++ AppUsuarios/views.py
@@ -184,6 +184,13 @@ def ejecutar_evaluacion(request):
         # return render(request, 'Usuarios/ejecutar_evaluacion.html', context)
     else:
         return redirect('home')
+    
+    
+    
+
+
+
+
 
 #*********************
 #       Cargos       *
